d29f105913954c4050883f798c58133a7e0c3c39
Tommy Hodgins <tomhodgins@gmail.com>
2018-12-24 15:52:00 -0500

    Refactor to be cleaner/faster

--- index.vanilla.js
+++ index.vanilla.js
@@ -1,17 +1,13 @@
 export default (selector, ancestor, rule) => {
-
-  return Array.from(document.querySelectorAll(ancestor))
-
+  const attr = (selector + ancestor).replace(/\W/g, '')
+  const result = Array.from(document.querySelectorAll(ancestor))
     .filter(tag => tag.querySelector(selector))
-
-    .reduce((styles, tag, count) => {
-
-      const attr = (selector + ancestor).replace(/\W/g, '')
-
-      tag.setAttribute(`data-ancestor-${attr}`, count)
-      styles += `${ancestor}[data-ancestor-${attr}="${count}"] { ${rule} }\n`
-      return styles
-
-    }, '')
-
+    .reduce((output, tag, count) => {
+        output.add.push({tag: tag, count: count})
+        output.styles.push(`${ancestor}[data-ancestor-${attr}="${count}"] { ${rule} }`)
+      return output
+    }, {add: [], remove: [], styles: []})
+  result.add.forEach(tag => tag.tag.setAttribute(`data-ancestor-${attr}`, tag.count))
+  result.remove.forEach(tag => tag.setAttribute(`data-ancestor-${attr}`, ''))
+  return result.styles.join('\n')
 }
--- source/ancestor.sexp
+++
@@ -1,7 +0,0 @@
-mixin('ancestor', ['selector', 'ancestor', 'rule'],
-  returnValue('Array.from(document.querySelectorAll(ancestor))',
-    filterFunc('tag.querySelector(selector)',
-      reduceFunc(
-        createAttribute(['selector', 'ancestor'],
-          addAttribute('tag', 'ancestor',
-            addRule('${ancestor}', '', 'ancestor')))))))
